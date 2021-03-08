a4ee9d17684e01a7be5dc503bce4e0c71361f8ab
Naman Bhatia <bhatia.naman@hotmail.com>
2021-03-08 19:30:56 +0530

    delete selected article only

--- index.js
+++ index.js
@@ -98,6 +98,19 @@ app.route("/articles/:articleTitle")
             }
         }
     );
+})
+
+.delete(function(req,res){
+    Article.deleteOne(
+        {title: req.params.articleTitle},
+        function(err){
+            if(!err){
+                res.send("Article deleted successfully")
+            }else{
+                res.send(err)
+            }
+        }
+    );
 });
 
 
